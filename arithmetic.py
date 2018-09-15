a856c23b051a8acad193cc743100ffb3fb0dfbaf
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-15 15:36:53 -0700

    defined arithmatic functions for calculator

--- arithmetic.py
+++ arithmetic.py
@@ -4,52 +4,40 @@
 def add(num1, num2):
     """Return the sum of the two inputs."""
 
-    return int(num1 + num2)
+    return (num1 + num2)
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
-    return int(num1 - num2)
-
+    return num1 - num2
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
-    return int(num1 * num2)
-
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
     return num1 / num2
 
-def power(num1, num2):
-    """Raise num1 to the power of num2 and return the value."""
-
-    return int(num1 ** num2)
-
 def square(num1):
     """Return the square of the input."""
 
-    return power(num1, 2)
-
+    return num1 ** 2
 
 def cube(num1):
     """Return the cube of the input."""
 
-    return power(num1, 3)
+    return num1 ** 3
 
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
 
-
-
+    return num1 ** num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
-    return int(num1 % num2)
-
-def add_mult(num1, num2, num3):
-    """Return the sum of the three inputs."""
-
-    return int(num1 + num2 + num3)
+    return num1 % num2
